53a0f8598addf4de0b8b879a53c0e028f41ad74b
Ivan <leontev.ivan2003@mail.ru>
2021-12-11 15:13:15 +0700

    delete php file

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 300,
-//         adaptiveHeight: true,
-//         slidesToShow: 1,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow-left.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow-right.png"></button>',
-//         responsive: [
-//           {
-//             breakpoint: 992,
-//             settings: {
-//               dots: true,
-//               arrows: false
-//             }
-//           }
-//         ]
-//       });
-// });
-
-
 const slider = tns({
   container: '.carousel__inner',
   items: 1,
